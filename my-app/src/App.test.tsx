fd01325211e0d286914aaacc99982e3157208df3
icedjello <bamfard@gmail.com>
2020-12-09 15:48:38 +0000

    Added nonDecreasing challenge.

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,12 +1,24 @@
 import {romanToInteger} from "./algorithms/romanNumerals";
-import {isNonDecreasing} from "./algorithms/nonDecreasing";
+import {isIncreasingWithAtMostOneException} from "./algorithms/nonDecreasing";
 
 test('roman numerals', () => {
-  expect(romanToInteger(`I`)).toBe(1);
-  expect(romanToInteger(`DCCCLXXXIX`)).toBe(889);
+    expect(romanToInteger(`I`)).toBe(1);
+    expect(romanToInteger(`MDCCCLXXXVIII`)).toBe(1888);
+    expect(romanToInteger(`DCCCLXXXIX`)).toBe(889);
+    expect(romanToInteger(`DCCLXXXIX`)).toBe(789);
+    expect(romanToInteger(`MCMXLIX`)).toBe(1949);
+    expect(romanToInteger(`MMXX`)).toBe(2020);
+
 });
 
-test ('non-decreasing array - simple', ()=>{
-  expect(isNonDecreasing([1,3])).toBe(true);
-  expect(isNonDecreasing([1,3,1])).toBe(false);
+test('non-decreasing array - simple', () => {
+    expect(isIncreasingWithAtMostOneException([1, 3])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 3, 5])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 3, 8, 7])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 3, 8, 4, 11, 10])).toBe(false);
+    expect(isIncreasingWithAtMostOneException([1])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 1])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 1, 2])).toBe(false);
+    expect(isIncreasingWithAtMostOneException([1, 1, 3])).toBe(true);
+    expect(isIncreasingWithAtMostOneException([1, 2, 3])).toBe(true);
 });
--- my-app/src/algorithms/nonDecreasing.ts
+++ my-app/src/algorithms/nonDecreasing.ts
@@ -1,3 +1,31 @@
-export function isNonDecreasing (input: number[]): boolean {
+export function isIncreasingWithAtMostOneException(inputArray: number[]): boolean {
+    let counter: number = 0;
+    let exceptionFound: boolean = false;
+    let searching: boolean = true;
+
+    let currentNumber: number = inputArray[0];
+    let nextNumber: number = inputArray[1];
+
+    while (searching) {
+        if (currentNumber >= nextNumber) { // number out of order
+            if (exceptionFound) {
+                return false;
+            }
+
+            exceptionFound = true;
+            counter++;
+            currentNumber = currentNumber+1;
+            nextNumber = inputArray[counter + 1];
+        } else {
+            counter++;
+            currentNumber = inputArray[counter];
+            nextNumber = inputArray[counter + 1];
+            if (counter >= inputArray.length -1) {
+                searching = false;
+            }
+        }
+
+    }
     return true;
 }
+
